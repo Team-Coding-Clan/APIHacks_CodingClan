fbe40f3960c2aeee946b193b3649c3c672312079
Tawishi <55306738+Tawishi@users.noreply.github.com>
2020-12-11 15:16:21 +0530

    Logical correction in formula

--- APIs/Kaya_value.py
+++ APIs/Kaya_value.py
@@ -1,29 +1,27 @@
 import wbdata as w
 
 country_code = input("Country code : ")
+
 emission_per_capita = w.get_data("EN.ATM.CO2E.EG.ZS", country=country_code.upper())
-for i in emission_per_capita:
-    if i['value'] != None :
-        emission_per_capita_value = i['value']
-        break
-#print(value)
-
-# country_code = input("Country code : ")
-GDP = w.get_data("NY.GDP.MKTP.CD", country=country_code.upper())
-GDP_val =  GDP[1]['value']
-# print(GDP[1]['value'])
-
-# country_code = input("Country code : ")
 energy_intensity = w.get_data("EG.EGY.PRIM.PP.KD", country=country_code.upper())
-#pprint(energy_intensity)
-for i in energy_intensity:
-    if i['value'] != None :
-        energy_intensity_val = i['value']
+data = w.get_data("SP.POP.TOTL", country=country_code.upper())
+GDP = w.get_data("NY.GDP.MKTP.CD", country=country_code.upper())
+
+for i,j,k,l in zip(emission_per_capita, energy_intensity, data, GDP) :
+    if(i['value']!=None and j['value']!=None and k['value']!=None and l['value']!=None and i['date']==j['date']==k['date']==l['date']) :
+#         print(i['date'],j['date'],k['date'],l['date'])
+#         print(i['value'])
+#         print()
+#         print(j['value'])
+#         print()
+#         print(k['value'])
+#         print()
+#         print(l['value'])
+        population = k['value']
+        emission_per_capita_value = i['value']
+        GDP_val = l['value']
+        energy_intensity_val = j['value']
         break
-#print(val)
-# print(emission_per_capita_value)
-# print(GDP_val)
-# print(energy_intensity_val)
-# print()
-kaya_value = emission_per_capita_value * GDP_val * energy_intensity_val
-print((kaya_value*0.001)/41.868,"metric tonne")
+kaya_value = (population*emission_per_capita_value * GDP_val * energy_intensity_val*0.001)/41.868
+print(kaya_value,"metric tonne")
+print(i['date'])
